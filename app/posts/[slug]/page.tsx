09c9523f6580f28443f17e28adc6f28f6e6d0806
Bridger Tower <bridgertower@gmail.com>
2024-05-29 11:44:54 -0600

    fix: typography on post page and cleaned up error

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import {
   getFeaturedMediaById,
   getAuthorById,
 } from "@/lib/wordpress";
+
 import { Section, Container, Article, Main } from "@/components/craft";
 import { Metadata } from "next";
 
@@ -32,13 +33,19 @@ export default async function Page({ params }: { params: { slug: string } }) {
         <Container>
           <BackButton />
           <h1 className="pt-12">{post.title.rendered}</h1>
-          <div className="text-black grid gap-4 mb-12">
+          <div className="grid gap-4 mb-12">
             <p>
-              By <a href={`/posts/authors/${author.slug}`}>{author.name}</a> |{" "}
+              {author.name && (
+                <span>
+                  By <a href={`/posts/authors/${author.slug}`}>{author.name}</a>{" "}
+                  |
+                </span>
+              )}{" "}
               {date}
             </p>
           </div>
           <div className="h-96 mb-12 md:h-[560px] overflow-hidden flex items-center justify-center border rounded-lg bg-accent/25">
+            {/* eslint-disable-next-line */}
             <img
               className="w-full"
               src={featuredMedia.source_url}
