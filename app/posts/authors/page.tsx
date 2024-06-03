3bd3921726eae49473726b274f751f2daa241119
Bridger Tower <bridgertower@gmail.com>
2024-06-03 08:56:28 -0600

    updates to name rendering for posts by author

--- app/posts/authors/page.tsx
+++ app/posts/authors/page.tsx
@@ -21,9 +21,11 @@ export default async function Page() {
         <h2>All Authors</h2>
         <div className="grid">
           {authors.map((author: any) => (
-            <Link key={author.id} href={`authors/${author.slug}`}>
-              {author.name}
-            </Link>
+            <Link
+              key={author.id}
+              href={`authors/${author.slug}`}
+              dangerouslySetInnerHTML={{ __html: author.name }}
+            ></Link>
           ))}
         </div>
       </Container>
