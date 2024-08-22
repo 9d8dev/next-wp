ca349d1b141d91adf61c6e9f7f726aca9c972957
Bridger Tower <bridgertower@gmail.com>
2024-08-22 10:02:03 -0600

    create dynamically generated sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from "next";
+import { getAllPosts } from "@/lib/wordpress";
+
+const siteConfig = {
+  site_domain: process.env.NEXT_PUBLIC_SITE_URL,
+  wordpress_url: process.env.WORDPRESS_URL,
+};
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getAllPosts();
+
+  const staticUrls: MetadataRoute.Sitemap = [
+    {
+      url: `${siteConfig.site_domain}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${siteConfig.site_domain}/posts`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${siteConfig.site_domain}/pages`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    // Add more static URLs as needed
+  ];
+
+  const postUrls: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `${siteConfig.site_domain}/posts/${post.slug}`,
+    lastModified: new Date(post.modified),
+    changeFrequency: "weekly",
+    priority: 0.5,
+  }));
+
+  return [...staticUrls, ...postUrls];
+}
