76032d8ade64e57815b20f89908f8bf826e66fc9
Bridger Tower <bridgertower@gmail.com>
2025-01-07 22:50:41 -0700

    feat: implement latest craft

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 import { Section, Container } from "@/components/craft";
 import Balancer from "react-wrap-balancer";
 
-// Components
+// Next.js Imports
 import Link from "next/link";
 
 // Icons
 import { File, Pen, Tag, Boxes, User, Folder } from "lucide-react";
+import { WordPressIcon } from "@/components/icons/wordpress";
+import { NextJsIcon } from "@/components/icons/nextjs";
 
 // This page is using the craft.tsx component and design system
 export default function Home() {
@@ -22,24 +24,11 @@ export default function Home() {
 // This is just some example JS to demonstrate automatic styling from brijr/craft
 const ExampleJsx = () => {
   return (
-    <main className="grid gap-12">
-      <div className="craft space-y-8">
+    <main className="space-y-8">
+      <div className="craft spaced">
         <h1>
           <Balancer>Headless WordPress built with the Next.js</Balancer>
         </h1>
-
-        {/* Vercel Clone Starter */}
-        <a
-          className="h-auto block"
-          href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F9d8dev%2Fnext-wp&env=WORDPRESS_URL,WORDPRESS_HOSTNAME&envDescription=Add%20WordPress%20URL%20with%20Rest%20API%20enabled%20(ie.%20https%3A%2F%2Fwp.example.com)%20abd%20the%20hostname%20for%20Image%20rendering%20in%20Next%20JS%20(ie.%20wp.example.com)&project-name=next-wp&repository-name=next-wp&demo-title=Next%20JS%20and%20WordPress%20Starter&demo-url=https%3A%2F%2Fwp.9d8.dev"
-        >
-          {/* eslint-disable-next-line */}
-          <img
-            className="not-prose my-4"
-            src="https://vercel.com/button"
-            alt="Deploy with Vercel"
-          />
-        </a>
 
         <p>
           This is <a href="https://github.com/9d8dev/next-wp">next-wp</a>,
@@ -52,6 +41,31 @@ const ExampleJsx = () => {
           typesafety is handled in <code>lib/wordpress.ts</code> and{" "}
           <code>lib/wordpress.d.ts</code>.
         </p>
+
+        <div className="flex justify-between items-center gap-4">
+          {/* Vercel Clone Starter */}
+          <div className="flex items-center gap-3">
+            <a
+              className="h-auto block"
+              href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F9d8dev%2Fnext-wp&env=WORDPRESS_URL,WORDPRESS_HOSTNAME&envDescription=Add%20WordPress%20URL%20with%20Rest%20API%20enabled%20(ie.%20https%3A%2F%2Fwp.example.com)%20abd%20the%20hostname%20for%20Image%20rendering%20in%20Next%20JS%20(ie.%20wp.example.com)&project-name=next-wp&repository-name=next-wp&demo-title=Next%20JS%20and%20WordPress%20Starter&demo-url=https%3A%2F%2Fwp.9d8.dev"
+            >
+              {/* eslint-disable-next-line */}
+              <img
+                className="not-prose my-4"
+                src="https://vercel.com/button"
+                alt="Deploy with Vercel"
+              />
+            </a>
+            <p className="!text-sm text-muted-foreground">
+              Deploy with Vercel in seconds.
+            </p>
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <WordPressIcon className="text-foreground" width={32} height={32} />
+            <NextJsIcon className="text-foreground" width={32} height={32} />
+          </div>
+        </div>
       </div>
 
       <div className="grid md:grid-cols-3 gap-4 mt-6">
