61135fdb8060deb2dd83fee20c00ce9982d71ea3
Bridger Tower <bridgertower@gmail.com>
2025-01-07 23:10:51 -0700

    feat: /admin redirect to wp-admin

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${process.env.WORDPRESS_HOSTNAME}`,
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: `${process.env.WORDPRESS_URL}/wp-admin`,
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
