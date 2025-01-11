075b1e2174518ef6f3fbc3fc3e890f3d8aa605f6
Bridger Tower <bridgertower@gmail.com>
2025-01-11 00:24:53 -0700

    example pages styling and metadata

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,8 +1,16 @@
 import { getAllPages } from "@/lib/wordpress";
 import { Section, Container, Prose } from "@/components/craft";
-
+import { Metadata } from "next";
 import BackButton from "@/components/back";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "All Pages",
+  description: "Browse all pages of our blog posts",
+  alternates: {
+    canonical: "/posts/pages",
+  },
+};
 
 export default async function Page() {
   const pages = await getAllPages();
@@ -10,17 +18,17 @@ export default async function Page() {
   return (
     <Section>
       <Container className="space-y-6">
-        <h2 className="sr-only">All Pages</h2>
-        <BackButton />
-        <Prose>
+        <Prose className="mb-8">
+          <h2>All Pages</h2>
           <ul className="grid">
             {pages.map((page: any) => (
               <li key={page.id}>
-                <Link href={`pages/${page.slug}`}>{page.title.rendered}</Link>
+                <Link href={`/pages/${page.slug}`}>{page.title.rendered}</Link>
               </li>
             ))}
           </ul>
         </Prose>
+        <BackButton />
       </Container>
     </Section>
   );
--- app/posts/authors/page.tsx
+++ app/posts/authors/page.tsx
@@ -1,15 +1,16 @@
 import { getAllAuthors } from "@/lib/wordpress";
-import { Section, Container } from "@/components/craft";
-import Link from "next/link";
-import BackButton from "@/components/back";
+import { Section, Container, Prose } from "@/components/craft";
 import { Metadata } from "next";
+import BackButton from "@/components/back";
+import Link from "next/link";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "All Authors",
-    description: "Browse all authors on the site.",
-  };
-}
+export const metadata: Metadata = {
+  title: "All Authors",
+  description: "Browse all authors of our blog posts",
+  alternates: {
+    canonical: "/posts/authors",
+  },
+};
 
 export default async function Page() {
   const authors = await getAllAuthors();
@@ -17,15 +18,17 @@ export default async function Page() {
   return (
     <Section>
       <Container className="space-y-6">
-        <h2>All Authors</h2>
+        <Prose className="mb-8">
+          <h2>All Authors</h2>
+          <ul className="grid">
+            {authors.map((author: any) => (
+              <li key={author.id}>
+                <Link href={`/posts/?author=${author.id}`}>{author.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </Prose>
         <BackButton />
-        <div className="grid craft">
-          {authors.map((author: any) => (
-            <Link key={author.id} href={`/posts/?author=${author.id}`}>
-              {author.name}
-            </Link>
-          ))}
-        </div>
       </Container>
     </Section>
   );
