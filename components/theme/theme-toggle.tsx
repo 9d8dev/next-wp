36c7cb045ce6c7ca4cb2057ccd8e2323786fe54b
Atharva Rane <atharvarane051102@gmail.com>
2025-02-03 13:17:01 +0530

    feat: improve theme toggle with hotkey support and enhanced ui

--- components/theme/theme-toggle.tsx
+++ components/theme/theme-toggle.tsx
@@ -1,27 +1,44 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-import { Button } from "@/components/ui/button";
+import * as React from "react"
+
+import { useTheme } from "next-themes"
+import { useHotkeys } from "react-hotkeys-hook"
+import { Moon, SunDim } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+type Theme = "light" | "dark" | "system"
+
+const KEYS = ["ctrl+u", "meta+u"]
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { setTheme, theme } = useTheme()
 
-  const handleClick = () => {
-    if (theme === "light") setTheme("dark");
-    else setTheme("light");
-  };
+  const handleChangeTheme = (theme: Theme) => {
+    // if (!document.startViewTransition) return setTheme(theme);
+    // document.startViewTransition(() => setTheme(theme));
+    setTheme(theme)
+  }
+
+  useHotkeys(KEYS, () => {
+    handleChangeTheme(theme === "light" ? "dark" : "light")
+  })
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={handleClick}
-      aria-label="Toggle theme"
-    >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    </Button>
-  );
+    <div className="fixed bottom-2 left-4 z-50">
+      <Button
+        variant="outline"
+        onKeyDown={() => handleChangeTheme(theme === "dark" ? "light" : "dark")}
+        onClick={() => handleChangeTheme(theme === "dark" ? "light" : "dark")}
+        size="icon"
+      >
+        {theme === "dark" ? (
+          <Moon className="size-5" />
+        ) : (
+          <SunDim className="size-5" />
+        )}
+      </Button>
+    </div>
+  )
 }
