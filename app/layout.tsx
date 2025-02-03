c808ed58c146ac423fdac7397b1a218123dd427c
Atharva Rane <atharvarane051102@gmail.com>
2025-02-03 13:19:34 +0530

    feat: update layout with new logo, font, and background gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
 import "./globals.css"
 
 import type { Metadata } from "next"
-import { Inter as FontSans } from "next/font/google"
+import { Plus_Jakarta_Sans } from "next/font/google"
+import Image from "next/image"
+import Link from "next/link"
+import Balancer from "react-wrap-balancer"
+import { Analytics } from "@vercel/analytics/react"
+
 import { ThemeProvider } from "@/components/theme/theme-provider"
 import { Button } from "@/components/ui/button"
 import { MobileNav } from "@/components/nav/mobile-nav"
 import { ThemeToggle } from "@/components/theme/theme-toggle"
-import { mainMenu, contentMenu } from "@/menu.config"
 import { Section, Container } from "@/components/craft"
-import { Analytics } from "@vercel/analytics/react"
-import { siteConfig } from "@/site.config"
-
-import Balancer from "react-wrap-balancer"
-import Logo from "@/public/logo.svg"
-import Image from "next/image"
-import Link from "next/link"
-
+import { Label } from "@/components/ui/label"
 import { cn } from "@/lib/utils"
+import { mainMenu, contentMenu } from "@/menu.config"
+import { siteConfig } from "@/site.config"
+import Logo from "@/public/logo.svg"
 
-const font = FontSans({
+const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
@@ -41,7 +41,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn("min-h-screen font-sans antialiased", font.variable)}>
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          plusJakartaSans.className,
+          "bg-[radial-gradient(at_68%_92%,rgba(151,71,255,0.8)_0px,transparent_50%),radial-gradient(at_35%_84%,rgba(255,183,3,0.9)_0px,transparent_50%)]"
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -66,21 +72,28 @@ const Nav = ({ className, children, id }: NavProps) => {
     >
       <div
         id="nav-container"
-        className="max-w-5xl mx-auto py-4 px-6 sm:px-8 flex justify-between items-center"
+        className="mx-auto py-4 px-6 sm:px-8 flex justify-between items-center"
       >
         <Link
           className="hover:opacity-75 transition-all flex gap-4 items-center"
           href="/"
         >
           <Image
-            src={Logo}
+            src="/images/goodfit.png"
             alt="Logo"
             loading="eager"
-            className="dark:invert"
-            width={42}
+            className="block dark:hidden"
+            width={96}
             height={26.44}
           />
-          <h2 className="text-sm">{siteConfig.site_name}</h2>
+          <Image
+            src="/images/goodfit-dark.png"
+            alt="Logo"
+            loading="eager"
+            className="hidden dark:block"
+            width={96}
+            height={26.44}
+          />
         </Link>
         {children}
         <div className="flex items-center gap-2">
@@ -93,9 +106,13 @@ const Nav = ({ className, children, id }: NavProps) => {
               </Button>
             ))}
           </div>
-          <Button asChild className="hidden sm:flex">
-            <Link href="https://app.goodfit.so">Get Started</Link>
-          </Button>
+          {/* sneaky trick to focus on the input element without javascript */}
+          <Label
+            className="flex justify-center items-center h-10 px-4 py-2 rounded-full bg-violet-500 text-white hover:bg-violet-600 transition-all"
+            htmlFor="email"
+          >
+            Request Early Access
+          </Label>
           <MobileNav />
         </div>
       </div>
