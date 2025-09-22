d6930087859afe09010a222dbdb21f3d47cbad79
Sergey Gorbachev <g.sergey.wolf@gmail.com>
2025-09-22 14:58:29 +0300

    change site domain and name in config

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ type SiteConfig = {
 };
 
 export const siteConfig: SiteConfig = {
-  site_name: "next-wp",
-  site_description: "Starter template for Headless WordPress with Next.js",
-  site_domain: "https://next-wp.com",
+  site_name: "Системный Блокъ",
+  site_description: "Онлайн-журнал о влиянии цифровых технологий на культуру, человека и общество",
+  site_domain: "https://new.sysblok.ru",
 };
