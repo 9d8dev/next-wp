584afb1428bf526eeaa09cc970ce96c3fa7e3bba
Sergey Gorbachev <g.sergey.wolf@gmail.com>
2025-09-22 17:30:42 +0300

    change default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,8 +45,7 @@ export default function RootLayout({
       <body className={cn("min-h-screen font-sans antialiased", font.variable)}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           <Nav />
