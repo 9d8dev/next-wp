9b174b8da42974b20bed7b9e0ba20469c1f17c4b
nik <75045642+toffyjar@users.noreply.github.com>
2025-10-03 12:23:33 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ type SiteConfig = {
 };
 
 export const siteConfig: SiteConfig = {
-  site_name: "next-wp",
-  site_description: "Starter template for Headless WordPress with Next.js",
-  site_domain: "https://next-wp.com",
+  site_name: "DapFlow",
+  site_description: "Headless WordPress with Next.js for DapFlow",
+  site_domain: "https://dapflow.com",
 };
