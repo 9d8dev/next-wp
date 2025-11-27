da5bec8dd683dd22b9bfc5a25077a9f1bbe33592
Bridger Tower <bridgertower@gmail.com>
2025-11-26 22:40:54 -0700

    remove wordpress folder

--- wordpress/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run the setup script in the background after a delay
-(sleep 30 && /usr/local/bin/setup-wordpress.sh) &
-
-# Run the original WordPress entrypoint
-exec docker-entrypoint.sh "$@"
